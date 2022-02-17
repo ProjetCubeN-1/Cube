88b2a6351ec71de1174a4ee1a9905fa7bafa9dac
Kyrian <73023906+Kyrian-C@users.noreply.github.com>
2022-02-17 15:29:29 +0100

    Add files via upload

---
+++ application/views/bonjour.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Cube</title>
+    <link rel="stylesheet" href="/vendor/coreui/coreui/dist/css/coreui.css">
+    <link rel="stylesheet" href="/vendor/coreui/coreui/dist/css/style.css">
+    <link rel="stylesheet" href="/vendor/fortawesome/font-awesome/css/all.min.css">
+    <script src="/vendor/@coreui/coreui/dist/js/coreui.bundle.js"></script>
+    <script src="/vendor/coreui/js/main.js"></script>
+    <script src="/vendor/coreui/coreui/dist/js/coreui.js"></script>
+    <script src="/vendor/coreui/coreui/dist/js/coreui.bundle.js"></script>
+    <script src="/vendor/components/jquery/jquery.min.js"></script>
+    <script src="/vendor/coreui/coreui/dist/js/coreui.bundle.min.js"></script>
+</head>
+
+<body>
+
+    <p>BONJOUR</p>
+</body>
+
+</html>
