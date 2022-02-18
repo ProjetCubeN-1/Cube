0673868d97e0081871c0bb75672d40f2fc1e31ec
Yoanngreys <yoann.greys05@gmail.com>
2022-02-18 15:31:01 +0100

    Revert "KK"
    This reverts commit b3b430965a105c8fb304c75f217cba5594e5a2e4.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,24 @@ class ComposerAutoloaderInite6841a60cb32b4c7eb7e89d273fbe0d7
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInite6841a60cb32b4c7eb7e89d273fbe0d7::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequiree6841a60cb32b4c7eb7e89d273fbe0d7($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequiree6841a60cb32b4c7eb7e89d273fbe0d7($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
