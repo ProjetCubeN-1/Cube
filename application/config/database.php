a7f857255ea30d9c480a81e11556e378bc551940
Kyrian-C <kyrian.chagnaud@gmail.com>
2022-06-01 11:46:49 +0200

    up modif projet

--- application/config/database.php
+++ application/config/database.php
@@ -73,7 +73,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-if (ENVIRONMENT == "production") { //je suis en developpement
+if (ENVIRONMENT != "production") { //je suis en developpement mais pour les test en ligne
 	$db['default'] = array(
 		'dsn'	=> '',
 		'hostname' => 'localhost',
