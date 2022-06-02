31464db2d9f55f8ee7dc3a5d8baacb91e3013f9a
Kyrian-C <kyrian.chagnaud@gmail.com>
2022-06-02 11:21:56 +0200

    script bdd

--- application/updates/001.php
+++ application/updates/001.php
@@ -1,13 +1,2 @@
 <?php
 $this->db->query('CREATE DATABASE IF NOT EXISTS cube_dev');
-
-$this->db->query('use cube_dev');
-
-$this->db->query('DROP TABLE IF EXISTS t_commentaires');
-$this->db->query('CREATE TABLE IF NOT EXISTS t_commentaires (
-    id_commentaire int(11) auto_increment primary key,
-    contenu varchar(500) NOT NULL,
-    date_commentaire date NOT NULL,
-    id_ressource int(11) NOT NULL,
-    id_utilisateur int(11) NOT NULL,
-    active int default 1)');
