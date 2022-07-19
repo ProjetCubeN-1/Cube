e6f4584998bff22866b1b3d4e5e4dedf8d45d745
Kyrian-C <kyrian.chagnaud@gmail.com>
2022-07-19 15:25:02 +0200

    Update topbar_connect.php

--- application/views/templates/topbar_connect.php
+++ application/views/templates/topbar_connect.php
@@ -1,8 +1,8 @@
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#03979e">
         <div class="container-fluid">
 
-            <button class="c-header-toggler c-class-toggler mfs-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true" data-bs-toggle="collapse" data-bs-target="#sidebar"><span class="navbar-toggler-icon"></span></button>
-            <ul class="c-header-nav d-md-down-none">
+            <button class="c-header-toggler c-class-toggler mfs-3 d-md-down" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true" data-bs-toggle="collapse" data-bs-target="#sidebar"><span class="navbar-toggler-icon"></span></button>
+            <ul class="c-header-nav d-md-down">
             </ul>
 
             <a href="/cube/accueil"><img class="ml-5" src="/img/logo.png" width="150" height="80"></a>
@@ -22,15 +22,15 @@
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             </div>
-        </div>
-<?php if($this->session->id !="5"){ ?>
+        <?php if ($this->session->id != "4") { ?>
             <a class="c-header-nav-link mr-4" href="/cube/creation_ressources">
                 <i class="fas fa-plus fa-lg" style="color:white;"></i>
             </a>
-<?php } ?>
-            
-            <a class="c-header-nav-link mr-3" href="/login/index">
-                <i class="fas fa-power-off fa-lg" style="color:white;"></i>
-            </a>
+        <?php } ?>
+
+        <a class="c-header-nav-link mr-3" href="/login/index">
+            <i class="fas fa-power-off fa-lg" style="color:white;"></i>
+        </a>
+        </div>
 
     </nav>
